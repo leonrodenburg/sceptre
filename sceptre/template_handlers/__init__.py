3d0247657b6d61a0da39bf1e6754f191469dcd12
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-11-04 13:54:12 +0100

    Add template handler base class and S3 implementation

--- sceptre/template_handlers/__init__.py
+++ sceptre/template_handlers/__init__.py
@@ -3,6 +3,7 @@ import abc
 import logging
 
 import six
+from jsonschema import validate
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -12,25 +13,28 @@ class TemplateHandler:
     by all Template Handlers.
 
     :param arguments: The arguments of the template handler
-    :type argument: object
-    :param stack: The associated stack of the template handler
-    :type stack: sceptre.stack.Stack
+    :type arguments: object
+    :param connection_manager: Connection manager used to call AWS
+    :type connection_manager: sceptre.connection_manager.ConnectionManager
     """
 
     __metaclass__ = abc.ABCMeta
 
-    def __init__(self, arguments=None, stack=None):
+    def __init__(self, arguments=None, connection_manager=None):
         self.logger = logging.getLogger(__name__)
         self.arguments = arguments
-        self.stack = stack
+        self.connection_manager = connection_manager
 
-    def setup(self):
+    @abc.abstractmethod
+    def schema(self):
         """
-        This method is called at during stack initialisation.
-        Implementation of this method in subclasses can be used to do any
-        initial setup of the object.
+        Returns the schema for the arguments of this Template Resolver. This will
+        be used to validate that the arguments passed in the stack config are what
+        the Template Handler expects.
+        :return: JSON schema that can be validated
+        :rtype: object
         """
-        pass  # pragma: no cover
+        pass
 
     @abc.abstractmethod
     def handle(self):
@@ -41,3 +45,11 @@ class TemplateHandler:
         can be interpreted by Sceptre (CloudFormation YAML / JSON, Jinja or Python)
         """
         pass  # pragma: no cover
+
+    def validate(self):
+        """
+        Validates if the current arguments are correct according to the schema.
+        :return: True if arguments valid, false if not
+        :rtype: bool
+        """
+        return validate(instance=self.arguments, schema=self.schema())
